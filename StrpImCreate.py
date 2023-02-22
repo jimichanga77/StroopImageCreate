d448dff6f79b97fe79c10eadebecf244a2aac3a3
jimichanga77 <81027925+jimichanga77@users.noreply.github.com>
2023-02-22 12:05:10 +0000

    Updated main script to add in loop for selecting colour (not working yet)
    Added in loop to create words and colour them automatically from csv file but not recognising the colour at the moment

--- StrpImCreate.py
+++ StrpImCreate.py
@@ -22,20 +22,21 @@ with open('StroopTrials.csv') as csv_file:
             colour = {row[2]} # prints the colour to the console
             print(word)
             print(colour)
+            #FOLLOWING CREATES THE IMAGE
+            img = np.zeros([300,300,3],dtype=np.uint8) # start creating an image (i.e. white image)
+            img.fill(255) # numpy array!
+            im = Image.fromarray(img) #convert numpy array to image
+            I1 = ImageDraw.Draw(im)
+            # Custom font style and font size
+            myFont = ImageFont.truetype("arial.ttf", 65) # specifies font and font size for words
+            # Add Text to an image, the first numbers are the location
+            I1.text((110, 110), word,font=myFont, fill=str(colour))
+            # Display edited image
+            im.show()
+            #im.save('REDgreen.jpg') # removed for now while checking
             line_count += 1
 
 
-img = np.zeros([300,300,3],dtype=np.uint8)
-img.fill(255) # numpy array!
-im = Image.fromarray(img) #convert numpy array to image
-I1 = ImageDraw.Draw(im)
-# Custom font style and font size
-myFont = ImageFont.truetype("arial.ttf", 65)
-# Add Text to an image, the first numbers are the location
-I1.text((110, 110), "red",font=myFont, fill="green")
-# Display edited image
-im.show()
-im.save('REDgreen.jpg')
 
 
 
