c75fae6a5e775b1ed4a95daca79caeede7333165
jimichanga77 <81027925+jimichanga77@users.noreply.github.com>
2023-02-22 15:13:26 +0000

    Update StrpImCreate.py
    Updated width and height details and made it so that text is anchored in the centre of the image. Width and height now specified at the start of the file.

--- StrpImCreate.py
+++ StrpImCreate.py
@@ -23,6 +23,8 @@ print(dirname)
 ### file_path = filedialog.askopenfilename()
 ##file_path = filedialog.askdirectory()
 
+width, height = 300, 300
+
 directory = os.path.join(dirname+"/")
 PATH = os.path.join(dirname+"/converted")
 if not os.path.exists(PATH):
@@ -47,14 +49,14 @@ with open(fileToOpen) as csv_file:
             print(word)
             print(colour)
             #FOLLOWING CREATES THE IMAGE
-            img = np.zeros([300,300,3],dtype=np.uint8) # start creating an image (i.e. white image)
+            img = np.zeros([width,height,3],dtype=np.uint8) # start creating an image (i.e. white image)
             img.fill(255) # numpy array!
             im = Image.fromarray(img) #convert numpy array to image
             I1 = ImageDraw.Draw(im)
             # Custom font style and font size
             myFont = ImageFont.truetype("arial.ttf", 65) # specifies font and font size for words
             # Add Text to an image, the first numbers are the location
-            I1.text((0, 0), word,font=myFont, fill=colour)
+            I1.text(((width)/2, (height)/2), word,font=myFont, fill=colour,anchor="mm", align='center')
             # Display edited image
             # im.show() # do not include this if you have a large list as it will create and show each image
             fnlist = [word.upper(),colour]
