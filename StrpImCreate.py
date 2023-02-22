389703e6abc67b8be12fb10f070468ff9dfb4aff
jimichanga77 <81027925+jimichanga77@users.noreply.github.com>
2023-02-22 10:12:30 +0000

    Add file dialogue and csv reader
    Added script to open file dialogue to choose file and to add in CSV reader

--- StrpImCreate.py
+++ StrpImCreate.py
@@ -2,6 +2,17 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import numpy as np
+import csv
+
+# open dialogue to select a file
+from tkinter.filedialog import askopenfilename
+filename = askopenfilename()
+print(filename)
+
+with open('employee_birthday.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+
 img = np.zeros([300,300,3],dtype=np.uint8)
 img.fill(255) # numpy array!
 im = Image.fromarray(img) #convert numpy array to image
