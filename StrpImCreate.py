9da430760ceceb2dda4c5bb781f498d6beefe042
jimichanga77 <81027925+jimichanga77@users.noreply.github.com>
2023-02-22 14:42:04 +0000

    Update StrpImCreate.py
    Updated to start creating filenames and paths for saving files to

--- StrpImCreate.py
+++ StrpImCreate.py
@@ -3,14 +3,37 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import numpy as np
 import csv
+import os
 
 # open dialogue to select a file
-#from tkinter.filedialog import askopenfilename
-#filename = askopenfilename()
-#print(filename)
+from tkinter.filedialog import askopenfilename
+fileToOpen = askopenfilename()
+print(fileToOpen)
 
-with open('StroopTrials.csv') as csv_file:
+dirname, filename = os.path.split(fileToOpen)
+
+print(dirname)
+
+### select a location for saving files
+##import tkinter as tk
+##from tkinter import filedialog
+##
+##root = tk.Tk()
+##root.withdraw()
+### file_path = filedialog.askopenfilename()
+##file_path = filedialog.askdirectory()
+
+directory = os.path.join(dirname+"/")
+PATH = os.path.join(dirname+"/converted")
+if not os.path.exists(PATH):
+    os.makedirs(PATH)
+os.mkdir(dirname + "/converted")
+print(directory)
+
+with open(fileToOpen) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+
+
 
     line_count = 0
     for row in csv_reader:
@@ -32,9 +55,14 @@ with open('StroopTrials.csv') as csv_file:
             # Add Text to an image, the first numbers are the location
             I1.text((110, 110), word,font=myFont, fill=colour)
             # Display edited image
-            im.show()
-            #im.save('REDgreen.jpg') # removed for now while checking
+            # im.show() # do not include this if you have a large list as it will create and show each image
+            fnlist = [word,colour]
+            fname="".join(fnlist)
+            print(fname)
+            im.save(directory+"converted/"+fname+".jpg") # save to a directory (should work)
+            #im.save(f"{file_path}\%s.jpg" % fname) # removed for now while checking
             line_count += 1
+            
 
 
 
