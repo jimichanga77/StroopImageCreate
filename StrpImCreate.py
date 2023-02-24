071325427bbc547333692c9f965738bab2ddded8
jimichanga77 <81027925+jimichanga77@users.noreply.github.com>
2023-02-24 15:36:37 +0000

    Updated StrpImCreate.py to create file list
    Updated main script so that it creates a csv trial list with new image names, placed in the folder with the images.

--- StrpImCreate.py
+++ StrpImCreate.py
@@ -13,7 +13,11 @@ print(fileToOpen)
 dirname, filename = os.path.split(fileToOpen)
 print(dirname)
 
-width, height = 400, 400
+###================Edit these parts for different sizes and image formats ====================================
+#Edit the width, height or extension details as needed
+width, height = 400, 400 #pop in some values for width and height of images
+ext = ".jpg" #can be .jpg, .png, etc
+###===========================================================================================================
 
 # FOLLOWING GETS THE DIRECTORY TO SAVE TO BASED ON THE LOCATION OF THE CSV FILE AND CREATES A FOLDER CALLED 'StimCreated' to store new stimuli (if one already exists it won't create one again)
 directory = os.path.join(dirname+"/")
@@ -27,18 +31,24 @@ print(directory)
 with open(fileToOpen) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
-
 # The following creates the images for each word in the csv file and adds in the colour as specified in another column
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}') # prints the column names for reference
             line_count += 1
+            row.append("image")
+            with open(r'StimCreated/trialsUpdated.csv','w') as f1: # need "a" and not w to append to a file, if not will overwrite
+                writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
+                writer.writerow(row)
         else:
+            check = row[4-1]
+            print(check)
             word = row[1] # prints the word to the console (NEED TO CHANGE THIS BASED ON COLUMNS IN CSV FILE)
             colour = row[2] # prints the colour to the console (NEED TO CHANGE THIS BASED ON COLUMNS IN CSV FILE)
             print(word)
             print(colour)
+            print(row)
             #FOLLOWING CREATES THE IMAGE
             img = np.zeros([width,height,3],dtype=np.uint8) # start creating an image (i.e. white image)
             img.fill(255) # numpy array create
@@ -52,11 +62,18 @@ with open(fileToOpen) as csv_file:
             # im.show() # do not include this if you have a large list as it will create and show each image
             fnlist = [word.upper(),colour]
             fname="".join(fnlist)
+            fnameFull = fname + ext
             print(fname)
-            im.save(directory+"StimCreated/"+fname+".jpg") # save to a directory (should work)
-            #im.save(f"{file_path}\%s.jpg" % fname) # removed for now while checking
+            print(fnameFull)
+            row.append(fnameFull)
+            print(row)
+            with open(r'StimCreated/trialsUpdated.csv','a') as f1: # need "a" and not w to append to a file, if not will overwrite
+                writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
+                writer.writerow(row)
+            im.save(directory+"StimCreated/"+fname+ext) # save to a directory 
             line_count += 1
-            
+
+    
 
 
 
