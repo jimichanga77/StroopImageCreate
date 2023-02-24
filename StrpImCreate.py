ed83cada6314b9160866ff78235f57ea0f1c00c3
jimichanga77 <81027925+jimichanga77@users.noreply.github.com>
2023-02-24 15:47:46 +0000

    Update main script to include column calc for words and colours
    Added in wordColumn indication and colourColumn indication so that this can be calculated automatically for row selection (e.g. colourColumn -1 later in the script)

--- StrpImCreate.py
+++ StrpImCreate.py
@@ -17,6 +17,8 @@ print(dirname)
 #Edit the width, height or extension details as needed
 width, height = 400, 400 #pop in some values for width and height of images
 ext = ".jpg" #can be .jpg, .png, etc
+wordColumn = 2 # The column in the spreadsheet where the words are listed (e.g. red, house, etc., in column 2 of the spreadsheet)
+colourColumn = 3 # The column in the spreadsheet where the colours are listed (e.g. red, green, blue, etc., in column 3 of the spreadsheet)
 ###===========================================================================================================
 
 # FOLLOWING GETS THE DIRECTORY TO SAVE TO BASED ON THE LOCATION OF THE CSV FILE AND CREATES A FOLDER CALLED 'StimCreated' to store new stimuli (if one already exists it won't create one again)
@@ -38,17 +40,15 @@ with open(fileToOpen) as csv_file:
             print(f'Column names are {", ".join(row)}') # prints the column names for reference
             line_count += 1
             row.append("image")
-            with open(r'StimCreated/trialsUpdated.csv','w') as f1: # need "a" and not w to append to a file, if not will overwrite
+            # The following opens the new trial list file and adds in the header, if a file already exists this will be overwritten
+            with open(r'StimCreated/trialsUpdated.csv','w') as f1: # set as 'w' to overwrite any previously existing version of the trialList to start new
                 writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
                 writer.writerow(row)
         else:
-            check = row[4-1]
-            print(check)
-            word = row[1] # prints the word to the console (NEED TO CHANGE THIS BASED ON COLUMNS IN CSV FILE)
-            colour = row[2] # prints the colour to the console (NEED TO CHANGE THIS BASED ON COLUMNS IN CSV FILE)
-            print(word)
-            print(colour)
-            print(row)
+            word = row[wordColumn-1] # This updates based on values placed at the top of the script
+            colour = row[colourColumn-1] # This updates based on values placed at the top of the script
+            print(word) # prints the word to the console
+            print(colour) # prints the colour of the ink to the console
             #FOLLOWING CREATES THE IMAGE
             img = np.zeros([width,height,3],dtype=np.uint8) # start creating an image (i.e. white image)
             img.fill(255) # numpy array create
@@ -67,9 +67,11 @@ with open(fileToOpen) as csv_file:
             print(fnameFull)
             row.append(fnameFull)
             print(row)
+            # The following opens the new trial list file and adds (appends) in the trial list details from the row and the image name
             with open(r'StimCreated/trialsUpdated.csv','a') as f1: # need "a" and not w to append to a file, if not will overwrite
                 writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
                 writer.writerow(row)
+            # the following saves the image to the directory
             im.save(directory+"StimCreated/"+fname+ext) # save to a directory 
             line_count += 1
 
